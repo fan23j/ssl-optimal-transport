6a892c8839a38be0a231297c9b6905c81cd9d705
Jack <fan23j@gmail.com>
2023-09-05 17:04:26 -0400

    fix bw

--- src/model/criterion/classify_anything_mixed.py
+++ src/model/criterion/classify_anything_mixed.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 from .asymmetric import AsymmetricLossOptimized
+from .utils import convert_targets
 
 
 class Classify_Anything_Mixed_Loss(nn.Module):
@@ -21,7 +22,16 @@ class Classify_Anything_Mixed_Loss(nn.Module):
             gamma_neg, gamma_pos, clip, eps, disable_torch_grad_focal_loss
         )
 
-    def forward(self, features, text_features, targets, dataset_indices, **kwargs):
+    def forward(
+        self,
+        features,
+        multilabel_text_features,
+        multiclass_text_features,
+        targets,
+        dataset_indices,
+        dataset,
+        **kwargs
+    ):
         """
         features: [B, 512]
         text_features: [num_class, 512]
@@ -29,24 +39,49 @@ class Classify_Anything_Mixed_Loss(nn.Module):
         dataset_indices: [B]
         """
 
-        cosim_matrix = torch.matmul(features, text_features.t()) / self.temperature
+        multilabel_sim_matrix = (
+            torch.matmul(features, multilabel_text_features.t()) / self.temperature
+        )
+        multiclass_sim_matrix = (
+            torch.matmul(features, multiclass_text_features.t()) / self.temperature
+        )
+
+        multiclass_targets, multilabel_targets = convert_targets(
+            targets,
+            dataset.mixed_indices,
+            dataset.multilabel_labels,
+            dataset.multiclass_labels,
+            dataset_indices,
+        )
 
         # Splitting data based on dataset_indices
         multiclass_indices = dataset_indices == 1
         multilabel_indices = dataset_indices == 0
 
-        multilabel_targets = targets[multilabel_indices.nonzero().squeeze().to("cpu")]
-        multiclass_targets = targets[multiclass_indices.nonzero().squeeze().to("cpu")]
+        multilabel_targets = multiclass_targets[
+            multilabel_indices.nonzero().squeeze().to("cpu")
+        ]
+        multiclass_targets = multilabel_targets[
+            multiclass_indices.nonzero().squeeze().to("cpu")
+        ]
 
-        multilabel_cosim_matrix = cosim_matrix[multilabel_indices.nonzero().squeeze()]
-        multiclass_cosim_matrix = cosim_matrix[multiclass_indices.nonzero().squeeze()]
+        multilabel_cosim_matrix = multilabel_sim_matrix[
+            multilabel_indices.nonzero().squeeze()
+        ]
+        multiclass_cosim_matrix = multiclass_sim_matrix[
+            multiclass_indices.nonzero().squeeze()
+        ]
 
         # Compute the multilabel loss using asym_loss
-        multilabel_loss = self.asym_loss(multilabel_cosim_matrix, multilabel_targets.to("cuda"))
+        multilabel_loss = self.asym_loss(
+            multilabel_cosim_matrix, multilabel_targets.to("cuda")
+        )
 
         # Convert multiclass one-hot targets to class labels for cross_entropy
         multiclass_labels = torch.argmax(multiclass_targets, dim=1)
-        multiclass_loss = F.cross_entropy(multiclass_cosim_matrix, multiclass_labels.to("cuda"))
+        multiclass_loss = F.cross_entropy(
+            multiclass_cosim_matrix, multiclass_labels.to("cuda")
+        )
 
         total_loss = multilabel_loss + multiclass_loss
 
@@ -57,4 +92,6 @@ class Classify_Anything_Mixed_Loss(nn.Module):
         }, [
             multilabel_cosim_matrix,
             multiclass_cosim_matrix,
+            multilabel_targets,
+            multiclass_targets,
         ]
