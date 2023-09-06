a3d7f909804bb51be6dffed4fd2ed62144c3ef5b
Jack Fan <fan23j@gmail.com>
2023-09-06 20:37:22 +0000

    test config

--- tools/train.sh
+++ tools/train.sh
@@ -1,2 +1,2 @@
-
-python train.py --cfg ../experiments/finetune/classify_anything_mixed_nuswide_cifar_batchwise.yaml
+python train.py --cfg ../experiments/finetune/test.yaml
+#python train.py --cfg ../experiments/finetune/classify_anything_mixed_nuswide_cifar_batchwise.yaml
