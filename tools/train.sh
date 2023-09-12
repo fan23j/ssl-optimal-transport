eee4f3acd2e68e24f0f957a2dba0e8051eb37f56
fan23 <fan23j@gmail.com>
2023-09-12 23:28:51 +0000

    im

--- tools/train.sh
+++ tools/train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python3 train.py --cfg ../experiments/finetune/classify_anything_coco_multi.yaml
+python train.py --cfg ../experiments/finetune/classify_anything_tiny_imagenet_single.yaml
